e46c9ba1f006a671db97659723f748a0787205af
Reynold Houle <reynold@sardius.media>
2024-02-05 14:01:15 -0500

    update comment

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run status on Elemental REST API
+# Run network settings endpoint on Elemental REST API
 curl_command="curl -X GET http://localhost/settings/network.json"
 
 # Execute the final command
