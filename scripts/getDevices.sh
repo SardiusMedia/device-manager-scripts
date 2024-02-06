dbefb9c79e82368316e565f6e3e0797c2de24c2e
Reynold Houle <reynold@sardius.media>
2024-02-06 09:45:33 -0500

    fix comment

--- scripts/getDevices.sh
+++ scripts/getDevices.sh
@@ -2,11 +2,7 @@ http://192.168.11.179/devices.json
 
 #!/bin/bash
 
-# Setup Stream Event Id Variable
-streamEventId="$1"
-
-# Perform variable substitution
-# Replace placeholders with the actual values
+# Gets input devices from Elemental 
 curl_command="curl -X POST http://localhost/api/devices.json"
 
 # Execute the final command
