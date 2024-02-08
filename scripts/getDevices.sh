c3a40a00c1fef7a23fed27ac7d7153b8c977fed9
Reynold Houle <reynold@sardius.media>
2024-02-08 14:55:54 -0500

    fix method of get devices

--- scripts/getDevices.sh
+++ scripts/getDevices.sh
@@ -3,7 +3,7 @@ http://192.168.11.179/devices.json
 #!/bin/bash
 
 # Run get input devices on Elemental REST API
-curl_command="curl -X POST http://localhost/api/devices.json"
+curl_command="curl -X GET http://localhost/api/devices.json"
 
 # Execute the final command
 eval "$curl_command"
