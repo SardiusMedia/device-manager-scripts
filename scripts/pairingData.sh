7b1e4ad60f34764547849268684009f818e5492e
Reynold Houle <reynold@sardius.media>
2024-02-09 16:06:55 -0500

    update script to proper stringified json

--- scripts/pairingData.sh
+++ scripts/pairingData.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Run status.sh script and print its output
-echo "Status Output:"
-/home/elemental/sardius/elScripts/status.sh
+# Run status.sh script and capture its output
+status_output=$(/home/elemental/sardius/elScripts/status.sh)
 
-# Run systemInfo.sh script and print its output
-echo "System Info Output:"
-/home/elemental/sardius/elScripts/systemInfo.sh
+# Run systemInfo.sh script and capture its output
+system_info_output=$(/home/elemental/sardius/elScripts/systemInfo.sh)
 
-# Run network.sh script and print its output
-echo "Devices Output:"
-/home/elemental/sardius/elScripts/network.sh
+# Run devices.sh script and capture its output
+devices_output=$(/home/elemental/sardius/elScripts/devices.sh)
+
+# Print the concatenated JSON string
+echo "{"
+echo "\"Status Output\": $status_output,"
+echo "\"System Info Output\": $system_info_output,"
+echo "\"Devices Output\": $devices_output"
+echo "}"
