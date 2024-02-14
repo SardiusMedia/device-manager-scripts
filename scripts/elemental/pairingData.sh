0bba0026ba2cd2638b43ad48abdaa77201c057e6
Reynold Houle <reynold@sardius.media>
2024-02-14 09:33:20 -0500

    add firmware to pairing data

--- scripts/elemental/pairingData.sh
+++ scripts/elemental/pairingData.sh
@@ -9,9 +9,13 @@ devices_output=$(/home/elemental/sardius/elScripts/getDevices.sh)
 # Run status.sh script and capture its output
 network_output=$(/home/elemental/sardius/elScripts/network.sh)
 
+# Run firmware.sh script and capture its output
+firmware_output=$(/home/elemental/sardius/elScripts/firmware.sh)
+
 # Print the concatenated JSON string
 echo -n "{"
 echo -n  "\"SystemInfo\": $system_info_output,"
 echo -n  "\"Devices\": $devices_output,"
-echo -n  "\"NetworkSettings\": $network_output"
+echo -n  "\"NetworkSettings\": $network_output,"
+echo -n  "\"Version\": $firmware_output"
 echo -n  "}"
