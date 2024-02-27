505a419cad47641f0f40fa7e45169a8f59844a02
Reynold Houle <reynold@sardius.media>
2024-02-27 16:17:12 -0500

    update event status

--- scripts/elemental/eventStatus.sh
+++ scripts/elemental/eventStatus.sh
@@ -2,10 +2,12 @@
 
 # Setup Stream Event Id Variable
 streamEventId="$1"
+# file extension determines status endpoint type
+extension="$2"
 
 # Perform variable substitution
 # Replace placeholders with the actual values
-curl_command="curl -X GET http://localhost/api/live_events/${streamEventId}/status.xml"
+curl_command="curl -X GET http://localhost/api/live_events/${streamEventId}/status.${extension}"
 
 # Execute the final command
 eval "$curl_command"
