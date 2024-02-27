eb147efc3c47f8c1402844954f0b76821e1284c1
Reynold Houle <reynold@sardius.media>
2024-02-27 14:53:24 -0500

    change event status to xml

--- scripts/elemental/eventStatus.sh
+++ scripts/elemental/eventStatus.sh
@@ -5,7 +5,7 @@ streamEventId="$1"
 
 # Perform variable substitution
 # Replace placeholders with the actual values
-curl_command="curl -X GET http://localhost/api/live_events/${streamEventId}/status.json"
+curl_command="curl -X GET http://localhost/api/live_events/${streamEventId}/status.xml"
 
 # Execute the final command
 eval "$curl_command"
