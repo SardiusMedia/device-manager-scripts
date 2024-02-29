064023d90a7822785c8c1655d8b230613ea284e1
Reynold Houle <reynold@sardius.media>
2024-02-29 08:32:26 -0500

    get all events

--- scripts/elemental/getAllEvents.sh
+++ scripts/elemental/getAllEvents.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run get input devices on Elemental REST API
-curl_command="curl -X GET http://localhost/api/devices.json"
+curl_command="curl -X GET http://localhost/api/live_events.json"
 
 # Execute the final command
 eval "$curl_command"
