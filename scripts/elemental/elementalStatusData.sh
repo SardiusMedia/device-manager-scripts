6be3d10c2150e12c2b7121e24acf6a9fe1ef5fca
Reynold Houle <reynold@sardius.media>
2024-03-07 10:41:36 -0500

    update script

--- scripts/elemental/elementalStatusData.sh
+++ scripts/elemental/elementalStatusData.sh
@@ -3,32 +3,6 @@
 # Presigned URL provided as the first command-line argument
 presigned_url="$1"
 
-# Function to convert XML to JSON using jq
-xml_to_json_jq() {
-    local xml="$1"
-    local json=$(jq -n --arg xml "$xml" '$xml | fromxml')
-    echo "$json"
-}
-
-# Check if jq is available, if not, install it
-if ! command -v jq &> /dev/null; then
-    echo "jq is not installed. Installing..."
-    
-    # Install jq using yum
-    yes | sudo yum install -y jq || {
-        echo "Unable to install jq. Please install it manually."
-        exit 1
-    }
-fi
-
-
-# Function to convert XML content to JSON
-convert_xml_to_json() {
-    local xml_content="$1"
-    local json_content=$(xml_to_json_jq "$xml_content")
-    echo "$json_content"
-}
-
 # Function to extract event IDs from JSON
 extract_event_ids() {
     local json="$1"
@@ -44,42 +18,47 @@ fetch_event_status() {
 }
 
 # Run systemInfo.sh script and capture its output
+echo "Fetching system status..."
 system_status_output=$(curl -sX GET http://localhost/system_status.json)
 
 # Run devices.sh script and capture its output
+echo "Fetching devices..."
 devices_output=$(curl -sX GET http://localhost/api/devices.json)
 
 # Run get input devices on Elemental REST API and capture the output
+echo "Fetching all events XML..."
 all_events_xml=$(curl -sX GET http://localhost/api/live_events.xml)
 
-# Convert XML to JSON
-all_events_json=$(convert_xml_to_json "$all_events_xml")
-
-# Extract event IDs from JSON
-event_ids=$(extract_event_ids "$all_events_json")
+# Extract event IDs from XML
+echo "Extracting event IDs..."
+event_ids=$(extract_event_ids "$all_events_xml")
 
 # Array to store event statuses
 event_statuses=()
 
 # Loop through each event ID and fetch its status
+echo "Fetching event statuses..."
 for event_id in $event_ids; do
+    echo "Fetching status for event ID: $event_id"
     event_status=$(fetch_event_status "$event_id")
     event_statuses+=("$event_status")
 done
 
 # Merge JSON responses into one object
-merged_json="{\"system_status\":$system_status_output, \"devices\":$devices_output, \"all_events\":$all_events_json, \"event_statuses\":${event_statuses[*]}}"
+merged_json="{\"system_status\":$system_status_output, \"devices\":$devices_output, \"all_events_xml\":\"$all_events_xml\", \"event_statuses\":${event_statuses[*]}}"
 
 # Calculate the length of the merged_json data
-content_length=$(echo -n "$merged_json" | wc -c)
+content_length="${#merged_json}"
 
 # Upload the merged_json to S3 using the presigned URL
+echo "Uploading merged JSON to S3..."
 curl -X PUT -T <(echo "$merged_json") -H "Content-Length: $content_length" -H "Transfer-Encoding:" "$presigned_url"
 
 # Extract the object key from the presigned URL
 object_key=$(echo "$presigned_url" | awk -F "/" '{print $(NF-1)}')
 
 # Return the object key through SSM
+echo "Setting object key: $object_key in SSM..."
 aws ssm put-parameter --name "ObjectKey" --value "$object_key" --type "String" --overwrite
 
 # Optionally, you may want to print the object key for debugging purposes
