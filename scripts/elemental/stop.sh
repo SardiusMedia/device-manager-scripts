c52078daf210b994cc29765b46883838b41247d6
Reynold Houle <reynold@sardius.media>
2024-03-11 10:30:12 -0400

    update stop script

--- scripts/elemental/stop.sh
+++ scripts/elemental/stop.sh
@@ -3,9 +3,19 @@
 # Setup Stream Event Id Variable
 streamEventId="$1"
 
-# Perform variable substitution
-# Replace placeholders with the actual values
-curl_command="curl -X POST http://localhost/api/live_events/${streamEventId}/stop -H 'Content-Type: application/xml' -H 'Accept: application/xml' -d '<stop></stop>'"
+# Perform variable substitution for stop command
+stop_output=$(curl -X POST http://localhost/api/live_events/${streamEventId}/stop.json -H 'Content-Type: application/xml' -H 'Accept: application/xml' -d '<stop></stop>')
 
-# Execute the final command
-eval "$curl_command"
+# Check if the stop was successful
+if [[ $stop_output == *"Event successfully stopped"* ]]; then
+    echo "Stop was successful"
+    # Perform variable substitution for delete command
+    delete_output=$(curl -X DELETE http://localhost/api/live_events/${streamEventId}.json)
+    if [[ $delete_output == *"Invalid command"* ]]; then
+        echo "Delete was unsuccessful: $delete_output"
+    else
+        echo "Delete executed"
+    fi
+else
+    echo "Stop was unsuccessful"
+fi
