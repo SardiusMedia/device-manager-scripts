bfce58a0f38404856d95629e22f5db727f2fd01b
Reynold Houle <reynold@sardius.media>
2024-03-15 14:09:39 -0400

    revert install

--- scripts/elemental/install.sh
+++ scripts/elemental/install.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-# Hardcoded parameters
-sourceUrl='https://raw.githubusercontent.com/SardiusMedia/device-manager-scripts/main/scripts/elemental'
-destinationFolder='/home/elemental/sardius/elScripts'
-scriptNames='systemInfo.sh network.sh firmware.sh getDevices.sh elementalStatusData.sh getAllEvents.sh pairingData.sh status.sh liveEvent.sh eventStatus.sh createLiveEvent.sh getLiveEvent.sh start.sh stop.sh'
-
 # Create directory structure
 sudo mkdir -p /home/elemental/sardius/ssm
-sudo mkdir -p "$destinationFolder"
+sudo mkdir -p /home/elemental/sardius/elScripts
 
 # Download the files
 sudo curl -o /home/elemental/sardius/ssm/amazon-ssm-agent.rpm https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-sudo curl -o "$destinationFolder/updateScripts.sh" "$sourceUrl/updateScripts.sh"
+sudo curl -o /home/elemental/sardius/elScripts/updateScripts.sh https://raw.githubusercontent.com/SardiusMedia/device-manager-scripts/main/scripts/elemental/updateScripts.sh
 
 # Make the script executable
-sudo chmod +x "$destinationFolder/updateScripts.sh"
-
-# Run the updateScripts.sh script
-"$destinationFolder/updateScripts.sh" "$sourceUrl" "$destinationFolder" "$scriptNames"
+sudo chmod +x /home/elemental/sardius/elScripts/updateScripts.sh
 
 # Install the package
 sudo yum install -y /home/elemental/sardius/ssm/amazon-ssm-agent.rpm
 
 # Stop the service
 sudo systemctl stop amazon-ssm-agent
+
