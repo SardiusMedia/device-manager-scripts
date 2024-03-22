b9228baecd574d7bf4e3d5c18950ca98f47112b8
Reynold Houle <reynold@sardius.media>
2024-03-22 16:26:29 -0400

    update outputs for install package

--- scripts/elemental/install.sh
+++ scripts/elemental/install.sh
@@ -11,9 +11,17 @@ sudo curl -o /home/elemental/sardius/elScripts/updateScripts.sh https://raw.gith
 # Make the script executable
 sudo chmod +x /home/elemental/sardius/elScripts/updateScripts.sh
 
-# Install the package, ignoring errors if already installed, automatically answering yes to prompts
+echo "Attempting to install Amazon SSM Agent..."
+
+# Install the package, automatically answering yes to prompts
 echo "yes" | sudo yum install -y /home/elemental/sardius/ssm/amazon-ssm-agent.rpm | true
 
+# Check if the installation was successful
+if [ $? -eq 0 ]; then
+    echo "Amazon SSM Agent installed successfully."
+else
+    echo "Amazon SSM Agent is already installed. Nothing to do."
+fi
 
 # Stop the service
 sudo systemctl stop amazon-ssm-agent
