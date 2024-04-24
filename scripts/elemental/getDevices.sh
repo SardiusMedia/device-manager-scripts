a2433a880303a76c95395c871b7d0cc314c95e5d
Reynold Houle <reynold@sardius.media>
2024-04-24 10:28:06 -0400

    getDevices calculate expires

--- scripts/elemental/getDevices.sh
+++ scripts/elemental/getDevices.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-# Username, user expiration, and user authentication key passed as arguments
+# Username and user authentication key passed as arguments
 username="$1"
-userExpire="$2"
-userAuthKey="$3"
+userAuthKey="$2"
+
+# Function to calculate the expiration time
+calculate_expires() {
+    local current_time=$(date -u +%s)  # Get the current time in Unix time
+    local expires=$((current_time + 30))  # Add 30 seconds to the current time
+    echo "$expires"
+}
 
 # Function to calculate the hashed key
 calculate_hashed_key() {
     local url="http://localhost/api/devices.json"
     local path_without_api_version=$(echo "$url" | sed -E 's/\/api[^\/]+//i')
-    local expires=$(( $(date -u +%s) + 30 ))
+    local expires=$(calculate_expires)  # Calculate the expiration time
     local hashed_key=$(echo -n "${userAuthKey}$(echo -n "${userAuthKey}${path_without_api_version}${username}${userAuthKey}${expires}" | md5sum | cut -d ' ' -f 1)" | md5sum | cut -d ' ' -f 1)
     echo "$hashed_key"
 }
-
 
 # Function to construct the CURL command with headers
 construct_curl_command() {
     local url="http://localhost/api/devices.json"
     local headers=""
-    if [[ -n "$username" && -n "$userExpire" && -n "$userAuthKey" ]]; then
-        # If username, userExpire, and userAuthKey are provided, set headers and use HTTPS
+    if [[ -n "$username" && -n "$userAuthKey" ]]; then
+        # If username and userAuthKey are provided, set headers and use HTTPS
         url="https://${url#http://}"
         local hashed_key=$(calculate_hashed_key)
-        headers="-H 'X-Auth-User: $username' -H 'X-Auth-Expires: $userExpire' -H 'X-Auth-Key: $hashed_key'"
+        local expires=$(calculate_expires)
+        headers="-H 'X-Auth-User: $username' -H 'X-Auth-Expires: $expires' -H 'X-Auth-Key: $hashed_key'"
     fi
     echo "curl -k -X GET $headers \"$url\""
 }
