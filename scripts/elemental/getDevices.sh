de461928bc2fb7c0548226d0daca33dbf6f1c65e
Reynold Houle <reynold@sardius.media>
2024-04-24 10:35:24 -0400

    add more to debug

--- scripts/elemental/getDevices.sh
+++ scripts/elemental/getDevices.sh
@@ -6,8 +6,9 @@ userAuthKey="$2"
 
 # Function to calculate the expiration time
 calculate_expires() {
-    local current_time=$(date -u +%s)  # Get the current time in Unix time
+    local current_time=$(date -u +%s)  # Get the current time in Unix time in UTC
     local expires=$((current_time + 30))  # Add 30 seconds to the current time
+    echo "Expiration time (UTC): $expires"
     echo "$expires"
 }
 
@@ -16,13 +17,11 @@ calculate_hashed_key() {
     local url="http://localhost/api/devices.json"
     local path_without_api_version=$(echo "$url" | sed -E 's/\/api[^\/]+//i')
     local expires=$(calculate_expires)  # Calculate the expiration time
-    echo "Expires: $expires"
     local concat_str="${userAuthKey}${path_without_api_version}${username}${userAuthKey}${expires}"
-    echo "Concatenated String: $concat_str"
     local md5_result=$(echo -n "$concat_str" | md5sum)
-    echo "MD5 Result: $md5_result"
     local hashed_key=$(echo -n "${userAuthKey}${md5_result}" | md5sum)
-    echo "Hashed Key: $hashed_key"
+    echo "MD5 result: $md5_result"
+    echo "Hashed key: $hashed_key"
     echo "$hashed_key" | cut -d ' ' -f 1
 }
 
@@ -36,12 +35,14 @@ construct_curl_command() {
         local hashed_key=$(calculate_hashed_key)
         local expires=$(calculate_expires)
         headers="-H 'X-Auth-User: $username' -H 'X-Auth-Expires: $expires' -H 'X-Auth-Key: $hashed_key'"
+        echo "Headers: $headers"
     fi
     echo "curl -k -X GET $headers \"$url\""
 }
 
 # Construct the CURL command
 curl_command=$(construct_curl_command)
+echo "CURL command: $curl_command"
 
 # Execute the final command
 eval "$curl_command"
