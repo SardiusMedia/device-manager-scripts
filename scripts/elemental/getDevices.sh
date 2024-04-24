6b6fc9261613a5a4cca91c4869ff544c0519257d
Reynold Houle <reynold@sardius.media>
2024-04-24 10:57:33 -0400

    test

--- scripts/elemental/getDevices.sh
+++ scripts/elemental/getDevices.sh
@@ -8,7 +8,6 @@ userAuthKey="$2"
 calculate_expires() {
     local current_time=$(date -u +%s)  # Get the current time in Unix time in UTC
     local expires=$((current_time + 30))  # Add 30 seconds to the current time
-    echo "Expiration time (UTC): $expires"
     echo "$expires"
 }
 
@@ -19,7 +18,6 @@ calculate_hashed_key() {
     local expires="$1"  # Expiration time passed as argument
     local concat_str="${userAuthKey}$(echo -n "${userAuthKey}${path_without_api_version}${username}${userAuthKey}${expires}" | md5sum | cut -d ' ' -f 1)"
     local hashed_key=$(echo -n "${userAuthKey}${concat_str}" | md5sum | cut -d ' ' -f 1)
-    echo "Hashed key: $hashed_key"
     echo "$hashed_key"
 }
 
@@ -33,7 +31,6 @@ construct_curl_command() {
         local expires=$(calculate_expires)  # Calculate the expiration time only once
         local hashed_key=$(calculate_hashed_key "$expires")  # Pass expiration time as argument
         headers="-H 'X-Auth-User: $username' -H 'X-Auth-Expires: $expires' -H 'X-Auth-Key: $hashed_key'"
-        echo "Headers: $headers"
     fi
     echo "curl -k -X GET $headers \"$url\""
 }
