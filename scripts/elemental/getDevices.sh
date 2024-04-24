01219ee745ca9dc363ff68a09920ac9b619bfe5a
Reynold Houle <reynold@sardius.media>
2024-04-24 11:26:09 -0400

    revert

--- scripts/elemental/getDevices.sh
+++ scripts/elemental/getDevices.sh
@@ -1,43 +1,22 @@
 #!/bin/bash
-
-# Username and user authentication key passed as arguments
 username="$1"
-userAuthKey="$2"
-
-# Function to calculate the expiration time
-calculate_expires() {
-    local current_time=$(date -u +%s)  # Get the current time in Unix time in UTC
-    local expires=$((current_time + 30))  # Add 30 seconds to the current time
-    echo "$expires"
-}
-
-# Function to calculate the hashed key
-calculate_hashed_key() {
-    local url="http://localhost/api/devices.json"
-    local path_without_api_version=$(echo "$url" | sed -E 's/\/api[^\/]+//i')
-    local expires="$1"  # Expiration time passed as argument
-    local concat_str="${userAuthKey}$(echo -n "${userAuthKey}${path_without_api_version}${username}${userAuthKey}${expires}" | md5sum | cut -d ' ' -f 1)"
-    local hashed_key=$(echo -n "${userAuthKey}${concat_str}" | md5sum | cut -d ' ' -f 1)
-    echo "$hashed_key"
-}
+userExpire="$2"
+userAuthKey="$3"
 
 # Function to construct the CURL command with headers
 construct_curl_command() {
     local url="http://localhost/api/devices.json"
     local headers=""
-    if [[ -n "$username" && -n "$userAuthKey" ]]; then
-        # If username and userAuthKey are provided, set headers and use HTTPS
+    if [[ -n "$username" && -n "$userExpire" && -n "$userAuthKey" ]]; then
+        # If username, userExpire, and userAuthKey are provided, set headers and use HTTPS
         url="https://${url#http://}"
-        local expires=$(calculate_expires)  # Calculate the expiration time only once
-        local hashed_key=$(calculate_hashed_key "$expires")  # Pass expiration time as argument
-        headers="-H 'X-Auth-User: $username' -H 'X-Auth-Expires: $expires' -H 'X-Auth-Key: $hashed_key'"
+        headers="-H 'X-Auth-User: $username' -H 'X-Auth-Expires: $userExpire' -H 'X-Auth-Key: $userAuthKey'"
     fi
-    echo "curl -k -X GET $headers \"$url\""
+    echo "curl -X GET $headers \"$url\""
 }
 
 # Construct the CURL command
 curl_command=$(construct_curl_command)
-echo "CURL command: $curl_command"
 
 # Execute the final command
 eval "$curl_command"
